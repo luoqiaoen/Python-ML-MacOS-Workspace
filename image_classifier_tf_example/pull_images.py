ae9be21dcdf7ad83b1118f23b3a18bd85866ada3
luoqiaoen <luoqiaoen@gmail.com>
2018-11-09 14:10:37 -0500

    20181109

--- image_classifier_tf_example/pull_images.py
+++ image_classifier_tf_example/pull_images.py
@@ -2,7 +2,7 @@ from google_images_download import google_images_download
 
 response = google_images_download.googleimagesdownload()
 
-arguments = {"keywords":"Rolex 216570", "size":"medium","print_urls":True}
+arguments = {"keywords":"Dogs", "size":"medium","print_urls":True}
 #creating list of arguments
 paths = response.download(arguments)   #passing the arguments to the function
 print(paths)   #printing absolute paths of the downloaded images
